8824813ec8709b9ffabb1b7fd7c84c9a8c84ec21
Hema bhagnani <hemabhagnani5@gmail.com>
2020-08-25 18:16:54 +0530

    Created User class object form FirebaseUser

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -30,7 +30,7 @@ class _SignInState extends State<SignIn> {
             else
               {
                 print("Signed in");
-                print(result);
+                print(result.Uid);
               }
           },
         ),
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,13 +1,19 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService{
   final FirebaseAuth _auth=FirebaseAuth.instance;
+  //create user obj from FirebaseUser
+  User _userFromFirebaseUser(FirebaseUser user)
+  {
+    return user!= null ? User(Uid:user.uid):null;
+  }
   //sign in anon
   Future signInAnon() async{
     try{
       AuthResult result=await _auth.signInAnonymously();
       FirebaseUser user=result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     }catch(e)
     {
       print(e.toString());
