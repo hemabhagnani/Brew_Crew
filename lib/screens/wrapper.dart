2841c8fef8058e11c11afbc9e057f40d585d5159
Hema bhagnani <hemabhagnani5@gmail.com>
2020-08-26 19:26:21 +0530

    using provider package to listen to stream

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,15 +1,15 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
 import 'package:brew_crew/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class Wrapper extends StatefulWidget {
-  @override
-  _WrapperState createState() => _WrapperState();
-}
+class Wrapper extends StatelessWidget {
 
-class _WrapperState extends State<Wrapper> {
   @override
   Widget build(BuildContext context) {
+    final user=Provider.of<User>(context);
+
     return Authenticate();
   }
 }
