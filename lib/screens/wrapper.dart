979b6c03ab34f294535c521581d1ff9ee385a9dc
Hema bhagnani <hemabhagnani5@gmail.com>
2020-08-27 19:40:26 +0530

    chng screen according to sign in or sign out

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -9,7 +9,9 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final user=Provider.of<User>(context);
-
+    if(user==null)
     return Authenticate();
+    else
+      return Home();
   }
 }
