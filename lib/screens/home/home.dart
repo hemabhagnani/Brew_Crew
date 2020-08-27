3419758be9e5de17f35a21045659ecaa54d53ab9
Hema bhagnani <hemabhagnani5@gmail.com>
2020-08-27 19:46:03 +0530

    Added AppBar to home screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -8,8 +8,15 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child:Text("home")
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        title: Text("Welcome"),
+        actions: <Widget>[
+          FlatButton.icon(onPressed: (){}, icon: Icon(Icons.person), label: Text("Logout"))
+        ],
+      ),
     );
   }
 }
