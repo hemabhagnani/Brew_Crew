7c0c70c055bfb56f27de1560488c0d4732d888dc
amitsaini4556 <amitsaini2701@gmail.com>
2020-09-02 21:43:30 +0530

    decorate sign-in form by using constant shared data(textInputDecoration).

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,6 @@
 import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:brew_crew/shared/constants.dart';
 
 class SignIn extends StatefulWidget {
   final Function toggleView;
@@ -39,6 +40,7 @@ class _SignInState extends State<SignIn> {
             children: <Widget>[
               SizedBox(height: 10.0),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Email'),
                 validator: (val) => val.isEmpty ? 'Enter an email' : null,
                 onChanged: (val){
                   setState(()=>email=val);
@@ -46,6 +48,7 @@ class _SignInState extends State<SignIn> {
               ),
               SizedBox(height: 10.0),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Password'),
               obscureText: true,
                 validator: (val) => val.length < 6 ? 'Enter a password 6+ chars long' : null,
                 onChanged: (val){
