f47fe2acc7eca2b2e44941f4e999bf22a9d7791b
amitsaini4556 <amitsaini2701@gmail.com>
2020-09-04 13:43:02 +0530

    define updateuserdata to update the user's data.

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,6 +1,17 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DataBase {
+  String uid;
+  DataBase({ this.uid });
+
   final brews = Firestore.instance.collection('brews');
+
+  Future updateUserData(String sugar , String name , int strength)
+  {
+    return brews.document(uid).setData( {
+      'sugar' : sugar,
+      'name' : name,
+      'strength' : strength
+    });
+  }
 }
