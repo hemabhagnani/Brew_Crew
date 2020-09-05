f9629b120b5dfeb59dc4db8bfdaddac35b7b753c
Hema bhagnani <hemabhagnani5@gmail.com>
2020-09-05 12:22:08 +0530

    Firestore record for new registered users

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -2,5 +2,17 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService{
   //reference to collection
+  final String uid;
+  //uid links the firebase user uid to the key to userdatabse
+  DatabaseService({this.uid});
   final CollectionReference brewCollection=Firestore.instance.collection("brews");
+  Future updateUserDatabase(String sugars,String name, String strength) async{
+    //create a newdocument using uid as key
+    return await brewCollection.document(uid).setData(
+        {'sugar': sugars,
+          'name': name,
+          'strength': strength,
+        }
+    );
+  }
 }
