51af01fc74c202e6dbab176f4a7d5aadfd329827
amitsaini4556 <amitsaini2701@gmail.com>
2020-09-07 17:03:19 +0530

    define QuerySnapshots for BrewS list

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,16 +1,33 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:brew_crew/models/brew.dart';
 
 class DataBase {
   String uid;
+
   DataBase({ this.uid });
 
-  final brews = Firestore.instance.collection('brews');
+  final _brew = Firestore.instance.collection('brews');
 
-  Future updateUserData(String sugar , String name , int strength) async {
-    return await brews.document(uid).setData( {
-      'sugar' : sugar,
-      'name' : name,
-      'strength' : strength
+  Future updateUserData(String sugar, String name, int strength) async {
+    return await _brew.document(uid).setData({
+      'sugar': sugar,
+      'name': name,
+      'strength': strength
     });
+  }
+
+  List<Brew>_brewFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((brew) {
+      return Brew(
+          name: brew.data['name'],
+          sugars: brew.data['sugars'],
+          strength: brew.data['strength']
+      );
+    }).toList();
+  }
+
+  Stream<List<Brew>> get brews {
+    return _brew.snapshots()
+        .map(_brewFromSnapshot);
   }
 }
