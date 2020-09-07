252df4ea1dcb11ab78ebc9b4596f50f931575aeb
amitsaini4556 <amitsaini2701@gmail.com>
2020-09-07 17:02:21 +0530

    Add StreamProvider

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,18 +1,26 @@
+import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/screens/home/brewlist.dart';
 import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/services/database.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        title: Text("Welcome"),
-        actions: <Widget>[
-          FlatButton.icon(onPressed: () async{ await _auth.signOut();}, icon: Icon(Icons.person), label: Text("Logout"))
-        ],
+    return StreamProvider<List<Brew>>.value(
+      value: DataBase().brews,
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          backgroundColor: Colors.brown[400],
+          title: Text("Welcome"),
+          actions: <Widget>[
+            FlatButton.icon(onPressed: () async{ await _auth.signOut();}, icon: Icon(Icons.person), label: Text("Logout"))
+          ],
+        ),
+        body: BrewList(),
       ),
     );
   }
